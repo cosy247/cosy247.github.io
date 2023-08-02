99397732a05de37a9c7f224e650c115629f7c4e8
lyq <705194673@qq.com>
2023-08-02 17:31:12 +0800

    使用vuepress2

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,35 +1,21 @@
-module.exports = {
-    permalink: '/:slug',
+import { defineUserConfig } from 'vuepress';
+
+export default defineUserConfig({
+    lang: 'zh-CN',
+    title: '你好， VuePres2s ！',
+    description: '这是我的第一个 VuePress 站点',
     themeConfig: {
-        logo: '/assets/img/logo.png',
-        nav: [
-            { text: 'Home', link: '/' },
-            { text: 'Guide', link: '/guide/' },
-            { text: 'External', link: 'https://google.com' },
-            {
-                text: 'Languages',
-                ariaLabel: 'Language Menu',
-                items: [
-                    { text: 'Chinese', link: '/language/chinese/' },
-                    { text: 'Japanese', link: '/language/japanese/' },
-                ],
-            },
-        ],
-        sidebar: [
-            {
-                title: 'Group 1', // 必要的
-                path: '/foo/', // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-                collapsable: false, // 可选的, 默认值是 true,
-                sidebarDepth: 1, // 可选的, 默认值是 1
-                children: ['/'],
-            },
-            {
-                title: 'Group 2',
-                children: [
-                    /* ... */
-                ],
-                initialOpenGroupIndex: -1, // 可选的, 默认值是 0
-            },
-        ],
+        search: true,
+        searchMaxSuggestions: 10,
     },
-};
+    themeConfig: {
+        search: false,
+        searchMaxSuggestions: 10,
+    },
+    themeConfig: {
+        algolia: {
+            apiKey: '<API_KEY>',
+            indexName: '<INDEX_NAME>',
+        },
+    },
+});
