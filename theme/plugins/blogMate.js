525c0b3a5fb69c1b1e0600349204c0b8fcbd2908
cosy247 <705194673@qq.com>
2023-08-21 19:55:23 +0800

    🦄perf(统计文件数据): 优化统计代码

--- theme/plugins/blogMate.js
+++ theme/plugins/blogMate.js
@@ -1,45 +1,68 @@
 export default ({ initMateNames = [], countMateNames = [], isArrMateNames = [] }) => ({
     name: 'plugins-blog-mate',
-    async onPrepared(app) {
-        const pageDatas = app.pages.map((page) => page.data);
+    onPrepared(app) {
+        const nowDate = Date.now();
+
         const countMateData = countMateNames.reduce((countMateData, mateName) => {
             countMateData[mateName] = {};
             return countMateData;
         }, {});
-        const pageMateData = [];
-        pageDatas.forEach(({ filePathRelative, key, path, frontmatter, git: { updatedTime, contributors } }) => {
-            if (filePathRelative) {
-                const commitNumber = contributors.reduce((commitNumber, { commits }) => commitNumber + commits, 0);
-                const itemMateData = { $: { key, path, updatedTime, commitNumber } };
-                initMateNames.forEach((mateName) => {
-                    if (!frontmatter[mateName]) {
-                        return;
-                    }
-                    itemMateData[mateName] = isArrMateNames.includes(mateName) ? frontmatter[mateName].split(/\s+/) : frontmatter[mateName];
-                }, {});
-                countMateNames.forEach((mateName) => {
-                    if (!frontmatter[mateName]) {
-                        return;
-                    }
+
+        const pageDatas = app.pages.reduce((pageDatas, page) => {
+            const {
+                filePathRelative,
+                frontmatter: mate,
+                data: { git = {} },
+            } = page;
+            const { createdTime = nowDate, updatedTime = nowDate, contributors = [] } = git;
+            const createdTimeString = new Date(createdTime).toLocaleString();
+            const updatedTimeString = new Date(updatedTime).toLocaleString();
+            const commitAmount = contributors.reduce((commitNumber, { commits }) => commitNumber + commits, 0);
+            const [type, fileName, moreFlag] = filePathRelative ? filePathRelative.split('/') : [];
+
+            // 判断路径是否正确
+            if (moreFlag || !fileName) return pageDatas;
+
+            // 数组属性转化
+            isArrMateNames.forEach((mateName) => {
+                if (mate[mateName]) {
+                    mate[mateName] = mate[mateName].split(' ');
+                }
+            });
+
+            // 属性计数
+            countMateNames.forEach((mateName) => {
+                const mateValue = mate[mateName];
+                if (mateValue) {
                     if (isArrMateNames.includes(mateName)) {
-                        frontmatter[mateName].split(/\s+/).forEach((mateValue) => {
-                            if (!countMateData[mateName][mateValue]) {
-                                countMateData[mateName][mateValue] = 0;
+                        mateValue.forEach((value) => {
+                            if (!countMateData[mateName][value]) {
+                                countMateData[mateName][value] = 0;
                             }
-                            countMateData[mateName][mateValue]++;
+                            countMateData[mateName][value]++;
                         });
                     } else {
-                        const mateValue = frontmatter[mateName];
                         if (!countMateData[mateName][mateValue]) {
                             countMateData[mateName][mateValue] = 0;
                         }
                         countMateData[mateName][mateValue]++;
                     }
-                }, {});
-                pageMateData.push(itemMateData);
-            }
-        });
+                }
+            });
 
-        app.writeTemp('blogMate.json', JSON.stringify({ pageMateData, countMateData }));
+            // 记录数据
+            if (!pageDatas[type]) {
+                pageDatas[type] = [];
+            }
+            pageDatas[type].push({
+                fileName,
+                mate,
+                $: { createdTime: createdTimeString, updatedTime: updatedTimeString, commitAmount },
+            });
+
+            return pageDatas;
+        }, {});
+
+        app.writeTemp('blogMate.json', JSON.stringify({ pageDatas, countMateData }));
     },
 });
