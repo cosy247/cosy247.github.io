0bad16d11e519772a2755cd4785a79b5cc4d2e81
lyq <705194673@qq.com>
2023-10-10 15:05:33 +0800

    🦄perf(入口文件): 优化代码

--- theme/Index.vue
+++ theme/Index.vue
@@ -3,24 +3,41 @@
     <Menu />
     <div class="main">
         <Home v-if="pageType == 'home'" />
-        <Blog v-else-if="pageType == 'blog'"/>
+        <Blog v-else-if="pageType == 'blog'" />
     </div>
     <Footer />
 </template>
 
 <script setup>
+    // 加载统一样式文件
     import './common/common.css';
+
+    // 加载组件
     import Footer from './components/Footer.vue';
     import Menu from './components/Menu.vue';
     import Home from './pages/Home.vue';
     import Blog from './pages/Blog.vue';
-    import { usePageData } from '@vuepress/client';
-    import stroe from './store'
 
-    const { path } = usePageData().value;
-    const pageType = path == '/' ? 'home' : path.split('/')[1];
-    
-    stroe.pageType = pageType;
+    // 其他
+    import { usePageData } from '@vuepress/client';
+    import stroe from './store';
+
+    /** 页面类型 */
+    const pageType = (() => {
+        const { path } = usePageData().value;
+        if (path == '/') {
+            stroe.pageType = 'home';
+        } else if (path == 'blog') {
+            stroe.pageType = 'blog';
+        } else if (path.match(/blog\//)) {
+            stroe.pageType = 'blog-item';
+        } else if (path == 'label') {
+            stroe.pageType = 'label';
+        } else if (path.match(/label\//)) {
+            stroe.pageType = 'label-item';
+        }
+        return stroe.pageType;
+    })();
 </script>
 
 <style scoped>
