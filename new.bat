b9cb0f2af31f4ea52ff74f6f4f8b23874c9d5589
lyq <705194673@qq.com>
2023-10-10 16:00:49 +0800

    ✨feat(模板文档生成脚本): 支持标题定义

--- new.bat
+++ new.bat
@@ -1,15 +1,24 @@
 @echo off
 chcp 65001
+setlocal EnableDelayedExpansion
 
 set tempFile=.\docs\%1\.md
 set toFile=.\docs\%1\%2.md
+set title=%2
+
+if "%3" neq "" set title=%3
 
 if exist %tempFile% (
 	if exist %toFile% (
-    	echo 已存在./docs/%1/%2.md数据文件!
+		echo 已存在/docs/%1/%2.md数据文件!
 	) else (
-		copy ".\docs\%1\.md" ".\docs\%1\%2.md"
+		for /f "tokens=*" %%a in (%tempFile%) do (
+			set line=%%a
+			set line=!line:{title}=%title%!
+			echo !line!>>%toFile%
+		)
+		echo 生成/docs/%1/%2.md文件成功!
 	)
 ) else (
-    echo 不存在./docs/%1/.md模板文件!
+    echo 不存在/docs/%1/.md模板文件!
 )
