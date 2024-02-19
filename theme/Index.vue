c3a24070cdb051e5f13d0261fd1d4a699b5c7623
lyq <705194673@qq.com>
2024-02-19 17:10:14 +0800

    暂存

--- theme/Index.vue
+++ theme/Index.vue
@@ -1,37 +1,37 @@
 <template>
-    <Menu />
-    <div class="main">
-        <Home v-if="pageType == 'home'" />
-        <Blog v-else-if="pageType == 'blog'" />
-    </div>
-    <Footer title="title" />
+  <Menu />
+  <div class="main">
+    <Home v-if="pageType == 'home'" />
+    <Blog v-else-if="pageType == 'blog'" />
+  </div>
+  <Footer title="title" />
 </template>
 
 <script setup>
-    import './styles/common.css';
-    import Footer from './components/Footer.vue';
-    import Menu from './components/Menu.vue';
-    import Home from './views/Home.vue';
-    import Blog from './views/Blog.vue';
-    import { usePageData } from '@vuepress/client';
+  import './styles/common.css';
+  import Footer from './components/Footer.vue';
+  import Menu from './components/Menu.vue';
+  import Home from './views/Home.vue';
+  import Blog from './views/Blog.vue';
+  import { usePageData } from '@vuepress/client';
 
-    /** 页面类型 */
-    const pageType = (() => {
-        const { path } = usePageData().value;
-        if (path == '/') {
-            return 'home';
-        } else if (path.startsWith('/blog/')) {
-            return 'blog';
-        }
-    })();
+  /** 页面类型 */
+  const pageType = (() => {
+    const { path } = usePageData().value;
+    if (path == '/') {
+      return 'home';
+    } else if (path.startsWith('/blog/')) {
+      return 'blog';
+    }
+  })();
 </script>
 
 <style scoped>
-    .main {
-        position: relative;
-        width: 100%;
-        min-height: 100%;
-        background: white;
-        z-index: 1;
-    }
+  .main {
+    position: relative;
+    width: 100%;
+    min-height: 100%;
+    background: var(--theme-background);
+    z-index: 1;
+  }
 </style>
