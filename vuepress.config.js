8256fdcd9e87f26108a34dae76135da5345d3921
cosy247 <705194673@qq.com>
2024-05-21 12:06:08 +0800

    主题入口函数

--- vuepress.config.js
+++ vuepress.config.js
@@ -1,16 +1,4 @@
 import cosyTheme from  "vuepress-theme-cosy";
 
 export default cosyTheme({
-  // 运行设置
-  theme: cosyTheme(),
-  temp: './.temp',
-  cache: './.cache',
-  public: './docs',
-  dest: './_CosyBlog',
-  permalinkPattern: ':raw',
-  // 网页信息设置
-  title: 'cosy247博客',
-  lang: 'zh-Hans-CN',
-  description: 'cosy247的个人博客 master',
-  head: [['link', { rel: 'icon', href: '/assets/logo.png' }]],
 });
